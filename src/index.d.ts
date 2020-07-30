9238badf4bbc2dd7d1982dd88d06c8f24fe49efb
kousaku-maron <kousaku-maron@users.noreply.github.com>
2020-07-30 16:26:58 +0000

    deploy: b0c58c32660a6387e3075276ef1f2604eeeb27f0

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,4 +5,5 @@ export * from './link';
 export * from './loader';
 export * from './image';
 export * from './card';
+export * from './separator';
 //# sourceMappingURL=index.d.ts.map
---
+++ src/separator.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+export declare type BaseSeparatorProps = {
+    layout?: 'vertical' | 'horizontal';
+} & React.HTMLAttributes<HTMLDivElement>;
+export declare const BaseSeparator: ({ layout, ...rest }: BaseSeparatorProps) => JSX.Element;
+export declare const Separator: (props: BaseSeparatorProps) => JSX.Element;
+//# sourceMappingURL=separator.d.ts.map
