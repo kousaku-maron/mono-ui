a84e82069ffdc9ca313b0de397a32ae3cc5a849a
kosaku kurino <kousaku.maron@gmail.com>
2020-07-30 12:25:27 +0900

    fix: remove app

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,6 +1,10 @@
 import './styles/reset.css'
 import React from 'react'
 import { render } from 'react-dom'
-import App from './app.tsx'
+// import App from './app'
+
+const App = () => {
+  return <p>aaaa</p>
+}
 
 render(<App />, document.getElementById('root'))
