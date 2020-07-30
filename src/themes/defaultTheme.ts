b6163d3ea9388297c1b1ffbbcb04055a2dd574d3
kosaku kurino <kousaku.maron@gmail.com>
2020-07-30 19:41:40 +0900

    feat: add button and theme

---
+++ src/themes/defaultTheme.ts
@@ -0,0 +1,66 @@
+const theme = {
+  tints: {
+    primary: {
+      main: '#6c5ce7',
+      light: '#6c5ce7',
+      dark: '#6c5ce7',
+    },
+    secondary: {
+      main: '#ffbd53',
+      light: '#ffef83',
+      dark: '#c88d21',
+    },
+    tertiary: {
+      main: '#ffbd53',
+      light: '#ffef83',
+      dark: '#c88d21',
+    },
+    quaternary: {
+      main: '#ffbd53',
+      light: '#ffef83',
+      dark: '#c88d21',
+    },
+    quinary: {
+      main: '#ffbd53',
+      light: '#ffef83',
+      dark: '#c88d21',
+    },
+    senary: {
+      main: '#ffbd53',
+      light: '#ffef83',
+      dark: '#c88d21',
+    },
+  },
+  foregrounds: {
+    primary: '#222222',
+    secondary: '#404040',
+    tertiary: '#7c7c7c',
+    placeholder: '#838383',
+    separator: '#d5d5d5',
+    onTintPrimary: '#ffffff',
+    onTintSecondary: '#ffffff',
+    onTintTertiary: '#ffffff',
+    onTintQuaternary: '#ffffff',
+    onTintQuinary: '#ffffff',
+    onTintSenary: '#ffffff',
+  },
+  backgrounds: {
+    primary: '#f8f8f8',
+    secondary: '#ffffff',
+    tertiary: '#ffffff',
+  },
+  system: {
+    blue: '#007aff',
+    red: '#ff6753',
+    white: '#ffffff',
+    black: '#000000',
+    gray: '#8e8e93',
+    gray2: '#aeaeb2',
+    gray3: '#c7c7cc',
+    gray4: '#d1d1d6',
+    gray5: '#e5e5ea',
+    gray6: '#f2f2f7',
+  },
+}
+
+export default theme
